6957c28a1969857e3e33f95bd87a4db91463115e
snowykte0426 <snowykte0426@naver.com>
2024-10-04 20:09:36 +0900

    feat: 로그인 엔드포인트 정의 및 구현
    로그인을 수행하는 엔드포인트 ``/signin``을 정의했습니다
    
    Resolves: #11

--- src/main/java/com/nextech/server/v1/domain/auth/controller/AuthController.java
+++ src/main/java/com/nextech/server/v1/domain/auth/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.nextech.server.v1.domain.auth.controller;
 
+import com.nextech.server.v1.domain.auth.dto.request.SignInRequest;
 import com.nextech.server.v1.domain.auth.dto.request.SignUpRequest;
 import com.nextech.server.v1.domain.auth.dto.response.SignUpResponse;
+import com.nextech.server.v1.domain.auth.service.SignInService;
 import com.nextech.server.v1.domain.auth.service.SignUpService;
+import com.nextech.server.v1.global.dto.response.TokenResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -18,10 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final SignUpService signUpService;
+    private final SignInService signInService;
 
     @Operation(summary = "SignUp", description = "회원가입")
     @PostMapping("/signup")
     public SignUpResponse signUp(@RequestBody SignUpRequest signUpRequest) {
         return signUpService.signUp(signUpRequest);
+    }
+
+    @Operation(summary = "Signin", description = "로그인")
+    @PostMapping("/signin")
+    public TokenResponse signIn(@RequestBody SignInRequest signInRequest) {
+        return signInService.signIn(signInRequest);
     }
 }
