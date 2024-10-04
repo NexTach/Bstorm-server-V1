9415d515ff9092c01cd99a2e649bef28825d80a6
snowykte0426 <snowykte0426@naver.com>
2024-10-04 14:08:16 +0900

    feat: 회원가입 엔드포인트 ``/signup`` 추가
    계정 회원가입을 수행하는 엔드포인트를 추가했습니다
    
    Resolves: #11
    Related to: #18

--- src/main/java/com/nextech/server/v1/domain/auth/controller/AuthController.java
+++ src/main/java/com/nextech/server/v1/domain/auth/controller/AuthController.java
@@ -1,4 +1,27 @@
 package com.nextech.server.v1.domain.auth.controller;
 
+import com.nextech.server.v1.domain.auth.dto.request.SignUpRequest;
+import com.nextech.server.v1.domain.auth.dto.response.SignUpResponse;
+import com.nextech.server.v1.domain.auth.service.SignUpService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "인증", description = "인증관련 API")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final SignUpService signUpService;
+
+    @Operation(summary = "SignUp", description = "회원가입")
+    @PostMapping("/signup")
+    public SignUpResponse signUp(@RequestBody SignUpRequest signUpRequest) {
+        return signUpService.signUp(signUpRequest);
+    }
 }
