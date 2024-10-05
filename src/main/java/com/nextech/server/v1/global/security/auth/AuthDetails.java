0304600676a08107ffbebd8f5e1cf807f1df5c9d
snowykte0426 <snowykte0426@naver.com>
2024-10-05 12:19:29 +0900

    feat: 계정 정보 조회 서비스 추가
    계정의 유효성을 검사하고 정보를 반환하는 AuthDetails 서비스를 작성했습니다
    
    Resolves: #11

---
+++ src/main/java/com/nextech/server/v1/global/security/auth/AuthDetails.java
@@ -0,0 +1,55 @@
+package com.nextech.server.v1.global.security.auth;
+
+import com.nextech.server.v1.domain.members.entity.Members;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class AuthDetails implements UserDetails {
+    private final Members member;
+
+    public AuthDetails(Members member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(member.getRole().name());
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(authority);
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
