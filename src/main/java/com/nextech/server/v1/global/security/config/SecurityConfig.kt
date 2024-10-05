2c44ef118ea3d1a8110a4f7ed69ba62490b77eae
snowykte0426 <snowykte0426@naver.com>
2024-10-05 13:18:05 +0900

    chore: CORS 활성화 및 JWT 필터 적용
    CORS를 활성화 하고 JWT 필터를 적용했습니다
    
    Resolves: #11

--- src/main/java/com/nextech/server/v1/global/security/config/SecurityConfig.kt
+++ src/main/java/com/nextech/server/v1/global/security/config/SecurityConfig.kt
@@ -1,13 +1,24 @@
 package com.nextech.server.v1.global.security.config
 
+import com.nextech.server.v1.global.security.filter.JwtFilter
+import com.nextech.server.v1.global.security.jwt.JwtProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
+@EnableWebSecurity
 @Configuration
-open class SecurityConfig {
+open class SecurityConfig(
+    private val jwtProvider: JwtProvider
+) {
 
     @Bean
     open fun passwordEncoder(): BCryptPasswordEncoder {
@@ -16,9 +27,26 @@ open class SecurityConfig {
 
     @Bean
     open fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf { csrf -> csrf.disable() }.authorizeHttpRequests { requests ->
+        http.csrf { it.disable() }.cors { it.configurationSource(corsConfigurationSource()) }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { requests ->
                 requests.requestMatchers("/auth/**").permitAll().anyRequest().authenticated()
-            }
+            }.addFilterBefore(JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
+    }
+
+    @Bean
+    open fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfig = CorsConfiguration().apply {
+            allowedOrigins = listOf("*")
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("Authorization", "Content-Type")
+            allowCredentials = false
+            exposedHeaders = listOf("Authorization")
+        }
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", corsConfig)
+        }
+        return source
     }
 }
