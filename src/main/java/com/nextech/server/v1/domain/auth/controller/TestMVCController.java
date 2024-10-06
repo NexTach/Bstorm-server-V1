8ff6570e2ac8d0959caccd6cd00a0f31cc428447
snowykte0426 <snowykte0426@naver.com>
2024-10-06 16:45:02 +0900

    refactor: Swagger Tag 어노테이션 추가
    Spring MVC 패턴으로 테스트를 위한 컨트롤러에 ``@Tag``,``@Operation`` 어노테이션을 추가하여 Swagger에 노출되도록 하였습니다
    
    Ref: #17
    Related to: #11, #31

--- src/main/java/com/nextech/server/v1/domain/auth/controller/TestMVCController.java
+++ src/main/java/com/nextech/server/v1/domain/auth/controller/TestMVCController.java
@@ -1,10 +1,15 @@
 package com.nextech.server.v1.domain.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Tag(name = "테스트", description = "테스트를 위한  SpringMVC Controller")
 @Controller
 public class TestMVCController {
+
+    @Operation(summary = "JWT Test", description = "JWT 토큰 테스트")
     @GetMapping("/test")
     public String test() {
         return "SecurityTest";
