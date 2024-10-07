be4c7f30dbc33cc2c7ccb903a7c6ebf4fbdb51ea
snowykte0426 <snowykte0426@naver.com>
2024-10-08 01:07:45 +0900

    feat: ``MembersInquiryResponse`` DTO 필드 추가
    관계정보를 반환하기 위한 필드를 추가했습니다
    
    Resolves: #18
    Related to: #39

--- src/main/java/com/nextech/server/v1/domain/members/dto/response/MembersInquiryResponse.kt
+++ src/main/java/com/nextech/server/v1/domain/members/dto/response/MembersInquiryResponse.kt
@@ -2,6 +2,7 @@ package com.nextech.server.v1.domain.members.dto.response
 
 import com.nextech.server.v1.global.enums.Gender
 import com.nextech.server.v1.global.enums.Roles
+import com.nextech.server.v1.global.members.dto.response.MembersInquiryListResponse
 
 data class MembersInquiryResponse(
     val id: Long,
@@ -10,5 +11,6 @@ data class MembersInquiryResponse(
     val gender: Gender,
     val role: Roles,
     val extentOfDementia: Int,
-    val profilePictureURI: String?
+    val profilePictureURI: String?,
+    val relation: MembersInquiryListResponse?
 )
