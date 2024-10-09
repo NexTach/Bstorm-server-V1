d6cbf83ba36265fbc11b04e4e6c382ed0a58d3ee
snowykte0426 <snowykte0426@naver.com>
2024-10-09 13:49:40 +0900

    refactor: 계정 정보 추출 메서드 분리
    Request 정보를 이용해 계정 정보를 추출하는 로직을 분리하였습니다
    
    Resolves: #18

---
+++ src/main/java/com/nextech/server/v1/global/members/service/MemberAuthService.kt
@@ -0,0 +1,27 @@
+package com.nextech.server.v1.global.members.service
+
+import com.nextech.server.v1.domain.members.entity.Members
+import com.nextech.server.v1.domain.members.repository.MemberRepository
+import com.nextech.server.v1.global.security.jwt.service.JwtAuthenticationService
+import com.nextech.server.v1.global.security.jwt.service.JwtTokenService
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class MemberAuthService(
+    private val jwtTokenService: JwtTokenService,
+    private val jwtAuthenticationService: JwtAuthenticationService,
+    private val memberRepository: MemberRepository
+) {
+
+    fun getMemberByToken(request: HttpServletRequest): Members? {
+        val token: String = jwtTokenService.resolveToken(request.getHeader("Authorization"))
+        val userInfo = jwtAuthenticationService.getAuthentication(token)
+        val member=memberRepository.findByPhoneNumber(userInfo.name)
+        member?.let {
+            return it
+        }
+        throw UsernameNotFoundException("User not found");
+    }
+}
