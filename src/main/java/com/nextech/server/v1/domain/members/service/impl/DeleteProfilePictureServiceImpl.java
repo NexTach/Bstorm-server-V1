e8f7d3ef5d1c72ac18b597fd31dfcc4329479946
snowykte0426 <snowykte0426@naver.com>
2024-10-09 18:19:58 +0900

    feat: 프로필 사진 삭제 로직 구현
    프로필 사진 파일을 삭제하는 로직을 구현했습니다
    
    Resolves: #18
    Related to: #25

---
+++ src/main/java/com/nextech/server/v1/domain/members/service/impl/DeleteProfilePictureServiceImpl.java
@@ -0,0 +1,42 @@
+package com.nextech.server.v1.domain.members.service.impl;
+
+import com.nextech.server.v1.domain.members.entity.Members;
+import com.nextech.server.v1.domain.members.repository.MemberRepository;
+import com.nextech.server.v1.domain.members.service.DeleteProfilePictureService;
+import com.nextech.server.v1.global.aws.service.FileDeleteService;
+import com.nextech.server.v1.global.exception.FileDeletionFailedException;
+import com.nextech.server.v1.global.exception.ProfilePictureNotFoundException;
+import com.nextech.server.v1.global.members.service.MemberAuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteProfilePictureServiceImpl implements DeleteProfilePictureService {
+
+    private final MemberAuthService memberAuthService;
+    private final FileDeleteService fileDeleteService;
+    private final MemberRepository memberRepository;
+    @Value("${AWS_S3_BUCKET}")
+    private String BUCKET_NAME;
+
+    @Override
+    @Transactional
+    public void deleteProfilePicture(HttpServletRequest request) {
+        Members member = memberAuthService.getMemberByToken(request);
+        if (member.getProfilePictureURI() == null && member.getProfilePictureName() == null) {
+            throw new ProfilePictureNotFoundException("Profile picture not found");
+        }
+        try {
+            fileDeleteService.deleteFile(member.getProfilePictureName(), BUCKET_NAME);
+        } catch (Exception e) {
+            throw new FileDeletionFailedException("Failed to delete profile picture");
+        }
+        member.setProfilePictureURI(null);
+        member.setProfilePictureName(null);
+        memberRepository.save(member);
+    }
+}
