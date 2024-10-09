3f1e407b754e721fae48869c07543f1242891dd9
snowykte0426 <snowykte0426@naver.com>
2024-10-09 13:48:44 +0900

    refactor: 파일 이름 일관성 유지
    계정을 ``Members`` 또는 ``Member``라고 명칭하는 것에서 벗어난 파일 명과 메서드명을 수정했습니다
    
    Resolves: #18

--- src/main/java/com/nextech/server/v1/domain/members/service/CurrentMemberInquiryService.java
+++ src/main/java/com/nextech/server/v1/domain/members/service/CurrentMemberInquiryService.java
@@ -3,6 +3,6 @@ package com.nextech.server.v1.domain.members.service;
 import com.nextech.server.v1.domain.members.dto.response.MembersInquiryResponse;
 import jakarta.servlet.http.HttpServletRequest;
 
-public interface CurrentUserInfoService {
-    MembersInquiryResponse getCurrentUserInfo(HttpServletRequest request);
+public interface CurrentMemberInquiryService {
+    MembersInquiryResponse getCurrentMemberInfo(HttpServletRequest request);
 }
