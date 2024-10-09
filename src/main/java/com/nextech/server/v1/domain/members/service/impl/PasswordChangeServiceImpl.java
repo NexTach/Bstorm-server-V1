7a7a26ccc5692cd82ea31c5befef3d64f775d49e
snowykte0426 <snowykte0426@naver.com>
2024-10-09 18:21:38 +0900

    feat: 비밀번호 변경 로직 구현
    계정의 비밀번호를 변경하는 로직을 구현했습니다
    
    Resolves: #18

---
+++ src/main/java/com/nextech/server/v1/domain/members/service/impl/PasswordChangeServiceImpl.java
@@ -0,0 +1,32 @@
+package com.nextech.server.v1.domain.members.service.impl;
+
+import com.nextech.server.v1.domain.auth.service.PasswordValidationService;
+import com.nextech.server.v1.domain.members.entity.Members;
+import com.nextech.server.v1.domain.members.repository.MemberRepository;
+import com.nextech.server.v1.domain.members.service.PasswordChangeService;
+import com.nextech.server.v1.global.members.service.MemberAuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordChangeServiceImpl implements PasswordChangeService {
+
+    private final MemberRepository memberRepository;
+    private final MemberAuthService memberAuthService;
+    private final PasswordValidationService passwordValidationService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    @Transactional
+    public void changePassword(HttpServletRequest request, String currentPassword, String password) {
+        Members member = memberAuthService.getMemberByToken(request);
+        passwordValidationService.validatePassword(member.getPassword(), currentPassword);
+        password = bCryptPasswordEncoder.encode(password);
+        member.setPassword(password);
+        memberRepository.save(member);
+    }
+}
