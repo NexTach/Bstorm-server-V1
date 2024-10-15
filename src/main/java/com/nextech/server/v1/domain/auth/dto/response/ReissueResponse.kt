d3caf470d49568a69c663d5a134047629895cd0a
snowykte0426 <snowykte0426@naver.com>
2024-10-15 14:53:33 +0900

    feat: ReissueResponse 정의
    기존 새로운 리프레시 토큰까지 발급하던 문제를 해결하기 위하여 재발급 엔드포인트의 반환값을 강제하도록 DTO를 정의했습니다
    
    Resolves: #42
    Related to: #11, #31

---
+++ src/main/java/com/nextech/server/v1/domain/auth/dto/response/ReissueResponse.kt
@@ -0,0 +1,10 @@
+package com.nextech.server.v1.domain.auth.dto.response
+
+import com.nextech.server.v1.global.enums.Roles
+import java.time.LocalDateTime
+
+data class ReissueResponse(
+    val accessToken: String,
+    val accessTokenExpiresIn: LocalDateTime,
+    val roles: Roles
+)
