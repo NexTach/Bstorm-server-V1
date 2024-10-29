472df7a6810f59fcd371af5a18d863e9ed3f7d9e
snowykte0426 <snowykte0426@naver.com>
2024-10-30 08:43:48 +0900

    feat: 관계 정보 반환 엔드포인트 정의
    사용자의 모든 관계정보를 조회하고 반환하는 엔드포인트를 정의하였습니다
    
    Resolves: #39

--- src/main/java/com/nextech/server/v1/domain/relation/controller/RelationController.java
+++ src/main/java/com/nextech/server/v1/domain/relation/controller/RelationController.java
@@ -1,13 +1,26 @@
 package com.nextech.server.v1.domain.relation.controller;
 
+import com.nextech.server.v1.domain.relation.dto.response.RelationInquiryListResponse;
+import com.nextech.server.v1.domain.relation.service.GetRelationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Tag(name = "Relation",description = "관계설정 관련 API")
+@Tag(name = "Relation", description = "관계설정 관련 API")
 @RequestMapping("/relation")
 @RequiredArgsConstructor
 public class RelationController {
+
+    private final GetRelationService getRelationService;
+
+    @Operation(summary = "GetRelation", description = "자신의 모든 관계을 조회합니다.")
+    @GetMapping
+    public RelationInquiryListResponse getRelation(HttpServletRequest request) {
+        return getRelationService.getRelation(request);
+    }
 }
