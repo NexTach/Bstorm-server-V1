6777375b82c0947f1c0f8625cb3f6fc8598ea549
snowykte0426 <snowykte0426@naver.com>
2024-10-30 08:42:45 +0900

    feat: 관계 정보 반환 DTO 정의
    일반적인 관계정보를 담을 수 있는 ``RelationInqueryResponse`` DTO와 이를 List로 담는 ``RelationInqueryListResponse`` DTO를 정의하였습니다
    
    Resolves: #39

---
+++ src/main/java/com/nextech/server/v1/domain/relation/dto/response/RelationInqueryResponse.kt
@@ -0,0 +1,9 @@
+package com.nextech.server.v1.domain.relation.dto.response
+
+import com.nextech.server.v1.domain.members.entity.Members
+
+data class RelationInqueryResponse(
+    val relationId: Long?,
+    var fromProtector: Members,
+    var toWard: List<Members>
+)
---
+++ src/main/java/com/nextech/server/v1/domain/relation/dto/response/RelationInquiryListResponse.kt
@@ -0,0 +1,5 @@
+package com.nextech.server.v1.domain.relation.dto.response
+
+data class RelationInquiryListResponse(
+    val relations: List<RelationInqueryResponse>
+)
