780fa6d80a750bc5041c4e345d91735f954daf7f
snowykte0426 <snowykte0426@naver.com>
2024-10-30 22:13:13 +0900

    feat: 관계 정보 저장 엔드포인트 정의
    관계 정보를 저장하는 엔드포인트 ``POST /api/v1/relation``를 정의하였습니다
    
    Resolves: #39

--- src/main/java/com/nextech/server/v1/domain/relation/controller/RelationController.java
+++ src/main/java/com/nextech/server/v1/domain/relation/controller/RelationController.java
@@ -1,14 +1,15 @@
 package com.nextech.server.v1.domain.relation.controller;
 
+import com.nextech.server.v1.domain.relation.dto.request.PostRelationRequest;
 import com.nextech.server.v1.domain.relation.dto.response.RelationInquiryListResponse;
 import com.nextech.server.v1.domain.relation.service.GetRelationService;
+import com.nextech.server.v1.domain.relation.service.PostRelationService;
+import com.nextech.server.v1.global.relation.entity.Relation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(name = "Relation", description = "관계설정 관련 API")
@@ -17,10 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class RelationController {
 
     private final GetRelationService getRelationService;
+    private final PostRelationService postRelationService;
 
-    @Operation(summary = "GetRelation", description = "자신의 모든 관계을 조회합니다.")
+    @Operation(summary = "GetRelation", description = "관계조회")
     @GetMapping
     public RelationInquiryListResponse getRelation(HttpServletRequest request) {
         return getRelationService.getRelation(request);
+    }
+
+    @Operation(summary = "PostRelation", description = "관계추가")
+    @PostMapping
+    public Relation postRelation(HttpServletRequest request, @RequestBody PostRelationRequest postRelationRequest) {
+        return postRelationService.postRelation(postRelationRequest);
     }
 }
