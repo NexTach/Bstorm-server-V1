574dfbce78816c96c51908646a9c285dec55077d
snowykte0426 <snowykte0426@naver.com>
2024-10-30 23:41:55 +0900

    feat: 전체 관계 제거 클래스 선언
    자신의 설정된 모든 관계를 제거하는 클래스를 선언하였습니다
    
    Resolves: #39

---
+++ src/main/java/com/nextech/server/v1/domain/relation/service/impl/AllDeleteRelationServiceImpl.java
@@ -0,0 +1,15 @@
+package com.nextech.server.v1.domain.relation.service.impl;
+
+import com.nextech.server.v1.domain.relation.service.AllDeleteRelationService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AllDeleteRelationServiceImpl implements AllDeleteRelationService {
+    @Override
+    public void deleteRelation(HttpServletRequest request) {
+        // TODO: implement
+    }
+}
