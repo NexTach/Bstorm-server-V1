bc5b9b4c2af24415efb26b97965880452c5a96c6
snowykte0426 <snowykte0426@naver.com>
2024-10-30 23:47:21 +0900

    feat: 관계 제거 엔드포인트 정의
    자신에게 설정된 관계를 제거하는 엔드포인트들을 정의했습니다
    
    Resolves: #39

--- src/main/java/com/nextech/server/v1/domain/relation/controller/RelationController.java
+++ src/main/java/com/nextech/server/v1/domain/relation/controller/RelationController.java
@@ -1,7 +1,10 @@
 package com.nextech.server.v1.domain.relation.controller;
 
+import com.nextech.server.v1.domain.relation.dto.request.DeleteRelationRequest;
 import com.nextech.server.v1.domain.relation.dto.request.PostRelationRequest;
 import com.nextech.server.v1.domain.relation.dto.response.RelationInquiryListResponse;
+import com.nextech.server.v1.domain.relation.service.AllDeleteRelationService;
+import com.nextech.server.v1.domain.relation.service.DeleteRelationService;
 import com.nextech.server.v1.domain.relation.service.GetRelationService;
 import com.nextech.server.v1.domain.relation.service.PostRelationService;
 import com.nextech.server.v1.global.relation.entity.Relation;
@@ -19,6 +22,8 @@ public class RelationController {
 
     private final GetRelationService getRelationService;
     private final PostRelationService postRelationService;
+    private final DeleteRelationService deleteRelationService;
+    private final AllDeleteRelationService allDeleteRelationService;
 
     @Operation(summary = "GetRelation", description = "관계조회")
     @GetMapping
@@ -30,5 +35,17 @@ public class RelationController {
     @PostMapping
     public Relation postRelation(@RequestBody PostRelationRequest postRelationRequest) {
         return postRelationService.postRelation(postRelationRequest);
+    }
+
+    @Operation(summary = "DeleteRelation", description = "관계삭제")
+    @DeleteMapping
+    public void deleteRelation(HttpServletRequest request, @RequestBody DeleteRelationRequest deleteRelationRequest) {
+        deleteRelationService.deleteRelation(request, deleteRelationRequest);
+    }
+
+    @Operation(summary = "AllDeleteRelation", description = "관계전체삭제")
+    @DeleteMapping("/all")
+    public void allDeleteRelation(HttpServletRequest request) {
+        allDeleteRelationService.deleteRelation(request);
     }
 }
