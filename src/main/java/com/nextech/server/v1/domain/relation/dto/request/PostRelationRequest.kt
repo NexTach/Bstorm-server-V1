87a84af3e6b4d2b1fe5c794a790362e634dde0f2
snowykte0426 <snowykte0426@naver.com>
2024-10-31 22:28:04 +0900

    fix: JSON 필드명 불일치 문제 해결
    클라이언트에서 API 요청시 ``toWard`` 대신 ``newWard``라는 필드명을 사용하도록 수정하였습니다
    
    Resolves: #39

--- src/main/java/com/nextech/server/v1/domain/relation/dto/request/PostRelationRequest.kt
+++ src/main/java/com/nextech/server/v1/domain/relation/dto/request/PostRelationRequest.kt
@@ -1,8 +1,9 @@
 package com.nextech.server.v1.domain.relation.dto.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotNull
 
 data class PostRelationRequest(
     @field:NotNull val fromProtector: String,
-    @field:NotNull val toWard: String
+    @field:NotNull @JsonProperty("newWard") val toWard: String
 )
