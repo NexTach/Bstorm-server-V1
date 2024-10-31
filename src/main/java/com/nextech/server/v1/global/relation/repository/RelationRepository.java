3be133f713cddf86dc310ce9e9e0016c32e4d143
snowykte0426 <snowykte0426@naver.com>
2024-10-31 16:20:22 +0900

    feat: 미사용 메서드 선언 제거 및 ``findByFromProtectedAndToWardContains`` 선언
    로직 개편에 따라 필요 없어진 메서드를 ``RelationRepository``에서 제거하였고 새롭게 필요해진 메서드를 선언하였습니다
    
    Resolves: #39

--- src/main/java/com/nextech/server/v1/global/relation/repository/RelationRepository.java
+++ src/main/java/com/nextech/server/v1/global/relation/repository/RelationRepository.java
@@ -1,6 +1,6 @@
 package com.nextech.server.v1.global.relation.repository;
 
-import com.nextech.server.v1.domain.members.entity.Members;
+import com.nextech.server.v1.global.members.entity.Members;
 import com.nextech.server.v1.global.relation.entity.Relation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +11,7 @@ import java.util.List;
 public interface RelationRepository extends JpaRepository<Relation, Long> {
     List<Relation> findByFromProtected(Members username);
     List<Relation> findByToWardContains(String phoneNumber);
+    List<Relation> findByFromProtectedAndToWardContains(Members fromProtected, String phoneNumber);
     void deleteByFromProtected(Members fromProtected);
     void deleteByToWardContains(String phoneNumber);
 }
