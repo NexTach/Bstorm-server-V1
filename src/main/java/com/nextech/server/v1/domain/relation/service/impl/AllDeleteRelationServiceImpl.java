cb34c25205c277d68ec6765601e984996cca972d
snowykte0426 <snowykte0426@naver.com>
2024-11-01 08:38:48 +0900

    feat: 관계 전체 삭제 로직 구현
    JWT를 이용해 사용자를 식별하고 해당 사용자의 모든 피보호자 관계를 삭제하는 로직을 구현하였습니다
    
    Resolves: #39

--- src/main/java/com/nextech/server/v1/domain/relation/service/impl/AllDeleteRelationServiceImpl.java
+++ src/main/java/com/nextech/server/v1/domain/relation/service/impl/AllDeleteRelationServiceImpl.java
@@ -1,17 +1,36 @@
 package com.nextech.server.v1.domain.relation.service.impl;
 
 import com.nextech.server.v1.domain.relation.service.AllDeleteRelationService;
+import com.nextech.server.v1.global.exception.RelationNullException;
+import com.nextech.server.v1.global.members.entity.Members;
+import com.nextech.server.v1.global.members.service.MemberAuthService;
+import com.nextech.server.v1.global.relation.entity.Relation;
+import com.nextech.server.v1.global.relation.repository.RelationRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class AllDeleteRelationServiceImpl implements AllDeleteRelationService {
+
+    private final MemberAuthService memberAuthService;
+    private final RelationRepository relationRepository;
+
     @Override
     public void deleteRelation(HttpServletRequest request) {
-        // TODO: implement
+        Members member = memberAuthService.getMemberByToken(request);
+        List<Relation>relations = relationRepository.findByFromProtected(member);
+        if (relations.isEmpty()) {
+            throw new RelationNullException("Relation does not exist.");
+        }
+        relations.forEach(relation -> {
+            relation.getToWard().clear();
+            relationRepository.save(relation);
+        });
     }
 }
