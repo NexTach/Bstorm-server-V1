c032ffb92679d06b4183e3195e4b46315aebdf43
snowykte0426 <snowykte0426@naver.com>
2024-11-03 13:42:10 +0900

    feat: 특정 리프레쉬 토큰 삭제 엔드포인트 정의
    특정 유저의 활성화된 모든 리프레쉬 토큰을 삭제하는 엔드포인트를 정의하였습니다
    
    Resolves: #27

--- src/main/java/com/nextech/server/v1/domain/admin/controller/AdminController.kt
+++ src/main/java/com/nextech/server/v1/domain/admin/controller/AdminController.kt
@@ -1,16 +1,26 @@
 package com.nextech.server.v1.domain.admin.controller
 
+import com.nextech.server.v1.domain.admin.dto.request.DeleteRefreshTokenRequest
+import com.nextech.server.v1.domain.admin.service.DeleteRefreshTokenService
 import com.nextech.server.v1.domain.admin.service.GetRefreshTokenService
 import com.nextech.server.v1.global.security.jwt.entity.RefreshToken
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/admin")
-class AdminController(private val getRefreshTokenService: GetRefreshTokenService) {
+class AdminController(
+    private val getRefreshTokenService: GetRefreshTokenService,
+    private val deleteRefreshTokenService: DeleteRefreshTokenService
+) {
     @GetMapping("/refreshtoken")
     fun refreshToken(): List<RefreshToken> {
         return getRefreshTokenService.getRefreshToken()
+    }
+
+    @DeleteMapping("/refreshtoken")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteRefreshToken(@RequestBody toUser: DeleteRefreshTokenRequest) {
+        deleteRefreshTokenService.deleteRefreshToken(toUser)
     }
 }
