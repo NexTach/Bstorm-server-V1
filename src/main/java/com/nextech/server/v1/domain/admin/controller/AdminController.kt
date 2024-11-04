a1bf66300986400adc722b83ba7c6b7d054c0f7a
snowykte0426 <snowykte0426@naver.com>
2024-11-04 16:37:11 +0900

    feat: ``DELETE /log`` 엔드포인트 정의
    ``deleteLog`` 메서드를 이용하는 ``DELETE /log`` 엔드포인트를 정의하였습니다
    
    Resolves: #27

--- src/main/java/com/nextech/server/v1/domain/admin/controller/AdminController.kt
+++ src/main/java/com/nextech/server/v1/domain/admin/controller/AdminController.kt
@@ -2,10 +2,7 @@ package com.nextech.server.v1.domain.admin.controller
 
 import com.nextech.server.v1.domain.admin.dto.request.DeleteRefreshTokenRequest
 import com.nextech.server.v1.domain.admin.dto.response.LogResponse
-import com.nextech.server.v1.domain.admin.service.AllDeleteRefreshTokenService
-import com.nextech.server.v1.domain.admin.service.DeleteRefreshTokenService
-import com.nextech.server.v1.domain.admin.service.GetLogService
-import com.nextech.server.v1.domain.admin.service.GetRefreshTokenService
+import com.nextech.server.v1.domain.admin.service.*
 import com.nextech.server.v1.global.security.jwt.entity.RefreshToken
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -16,7 +13,8 @@ class AdminController(
     private val getRefreshTokenService: GetRefreshTokenService,
     private val deleteRefreshTokenService: DeleteRefreshTokenService,
     private val allDeleteRefreshTokenService: AllDeleteRefreshTokenService,
-    private val getLogService: GetLogService
+    private val getLogService: GetLogService,
+    private val deleteLogService: DeleteLogService
 ) {
     @GetMapping("/refreshtoken")
     fun refreshToken(): List<RefreshToken> {
@@ -38,5 +36,11 @@ class AdminController(
     @GetMapping("/log")
     fun getLog(): List<LogResponse> {
         return getLogService.getLog()
+    }
+
+    @DeleteMapping("/log")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteLog() {
+        deleteLogService.deleteLog()
     }
 }
