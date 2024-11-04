681558f32055d45515e4ef5fb0afb23118df63cf
snowykte0426 <snowykte0426@naver.com>
2024-11-04 11:26:37 +0900

    feat: ``DELETE /refreshtoken/all``,``GET /log`` 엔드포인트 선언
    로그아웃 기능이 불필요하다 판단됨에 따라 ``SignOut`` 관련 메서드들과 클래스들을 삭제하였습니다
    
    Resolves: #27
    Related to: #28

--- src/main/java/com/nextech/server/v1/domain/admin/controller/AdminController.kt
+++ src/main/java/com/nextech/server/v1/domain/admin/controller/AdminController.kt
@@ -1,6 +1,7 @@
 package com.nextech.server.v1.domain.admin.controller
 
 import com.nextech.server.v1.domain.admin.dto.request.DeleteRefreshTokenRequest
+import com.nextech.server.v1.domain.admin.service.AllDeleteRefreshTokenService
 import com.nextech.server.v1.domain.admin.service.DeleteRefreshTokenService
 import com.nextech.server.v1.domain.admin.service.GetRefreshTokenService
 import com.nextech.server.v1.global.security.jwt.entity.RefreshToken
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/admin")
 class AdminController(
     private val getRefreshTokenService: GetRefreshTokenService,
-    private val deleteRefreshTokenService: DeleteRefreshTokenService
+    private val deleteRefreshTokenService: DeleteRefreshTokenService,
+    private val allDeleteRefreshTokenService: AllDeleteRefreshTokenService
 ) {
     @GetMapping("/refreshtoken")
     fun refreshToken(): List<RefreshToken> {
@@ -22,5 +24,16 @@ class AdminController(
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteRefreshToken(@RequestBody toUser: DeleteRefreshTokenRequest) {
         deleteRefreshTokenService.deleteRefreshToken(toUser)
+    }
+
+    @DeleteMapping("/refreshtoken/all")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun allDeleteRefreshToken() {
+        allDeleteRefreshTokenService.allDeleteRefreshToken()
+    }
+
+    @GetMapping("/log")
+    fun log(): String {
+        TODO()
     }
 }
