125a821481fca170cf169bafe0e4e99a06dea54d
snowykte0426 <snowykte0426@naver.com>
2024-11-11 23:40:34 +0900

    feat: ``@EnableAsync`` 어노테이션 적용
    비동기 처리를 적용하기 위해 ``@EnableAsync`` 어노테이션을 적용하였습니다
    
    Related to: #16, #25

--- src/main/java/com/nextech/server/v1/BstormServerV1Application.java
+++ src/main/java/com/nextech/server/v1/BstormServerV1Application.java
@@ -3,11 +3,12 @@ package com.nextech.server.v1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @EnableRedisRepositories(basePackages = "com.nextech.server.v1.global.security.jwt.repository")
 public class BstormServerV1Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BstormServerV1Application.class, args);
 	}
