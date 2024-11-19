830ba1cda883f181acd8be41b5bbf004a8fc692d
김승찬 <s24041@gsm.hs.kr>
2024-11-19 22:36:14 +0900

    feat: MissionDto 정의

---
+++ src/main/java/com/nextech/server/v1/domain/mission/dto/mission/MissionDto.java
@@ -0,0 +1,25 @@
+package com.nextech.server.v1.domain.mission.dto.mission;
+
+import com.nextech.server.v1.domain.mission.dto.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MissionDto {
+
+    private int id;
+    private String from;
+    private List<Status> status;
+    private String toWard;
+    private LocalDateTime startDate;
+    private LocalDateTime expirationDate;
+    private String title;
+    private String content;
+
+}
