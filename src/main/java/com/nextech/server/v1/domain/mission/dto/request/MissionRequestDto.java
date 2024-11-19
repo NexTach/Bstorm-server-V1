683619fc4c94a9d9780759e0d35c6d64bd8e31ab
김승찬 <s24041@gsm.hs.kr>
2024-11-19 22:36:23 +0900

    feat: MissionRequestDto 정의

---
+++ src/main/java/com/nextech/server/v1/domain/mission/dto/request/MissionRequestDto.java
@@ -0,0 +1,13 @@
+package com.nextech.server.v1.domain.mission.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MissionRequestDto {
+
+    private String authorization;
+}
