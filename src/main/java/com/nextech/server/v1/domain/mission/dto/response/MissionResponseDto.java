c1b0aba0de2e33e073d78904601988348993738d
김승찬 <s24041@gsm.hs.kr>
2024-11-24 15:38:14 +0900

    feat: MissionResponsetDto 설정

--- src/main/java/com/nextech/server/v1/domain/mission/dto/response/MissionResponseDto.java
+++ src/main/java/com/nextech/server/v1/domain/mission/dto/response/MissionResponseDto.java
@@ -2,15 +2,35 @@ package com.nextech.server.v1.domain.mission.dto.response;
 
 import com.nextech.server.v1.domain.mission.entity.Mission;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MissionResponseDto {
 
-    private List<Mission> body;
+    private Long id;
+    private String from;
+    private String toWard;
+    private String status;
+    private String startDate;
+    private String expirationDate;
+    private String title;
+    private String content;
+
+    public static MissionResponseDto from(Mission mission) {
+        return MissionResponseDto.builder()
+                .id(mission.getId())
+                .from(mission.getFrom().getName())
+                .toWard(mission.getToWard().getName())
+                .status(mission.getStatus().toString())
+                .startDate(mission.getStartDate().toString())
+                .expirationDate(mission.getExpirationDate().toString())
+                .title(mission.getTitle())
+                .content(mission.getContent())
+                .build();
+    }
 }
