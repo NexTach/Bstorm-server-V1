ca10ba51dadb4d8db7e75a73a37f8a7bf0a917ee
김승찬 <s24041@gsm.hs.kr>
2024-11-25 12:18:21 +0900

    feat: 미션등록 서비스 추가

--- src/main/java/com/nextech/server/v1/domain/mission/service/impl/MissionCreateServiceImpl.java
+++ src/main/java/com/nextech/server/v1/domain/mission/service/impl/MissionCreateServiceImpl.java
@@ -4,34 +4,21 @@ import com.nextech.server.v1.domain.members.repository.MembersRepository;
 import com.nextech.server.v1.domain.mission.dto.request.MissionRequestDto;
 import com.nextech.server.v1.domain.mission.dto.response.MissionResponseDto;
 import com.nextech.server.v1.domain.mission.entity.Mission;
+import com.nextech.server.v1.domain.mission.service.MissionCreateService;
 import com.nextech.server.v1.domain.mission.repository.MissionRepository;
-import com.nextech.server.v1.domain.mission.service.MissionService;
 import com.nextech.server.v1.global.exception.LogNotFoundException;
 import com.nextech.server.v1.global.members.entity.Members;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
+@Transactional
 @RequiredArgsConstructor
-public class MissionServiceImpl implements MissionService {
+public class MissionCreateServiceImpl implements MissionCreateService {
 
     private final MissionRepository missionRepository;
     private final MembersRepository membersRepository;
-
-    @Override
-    public List<MissionResponseDto> getAllMissions() {
-        List<Mission> missions = missionRepository.findAll();
-        if (missions.isEmpty()) {
-            throw new LogNotFoundException("No missions found");
-        }
-
-        return missions.stream()
-                .map(MissionResponseDto::from)
-                .collect(Collectors.toList());
-    }
 
     @Override
     public MissionResponseDto createMission(MissionRequestDto missionRequestDto) {
