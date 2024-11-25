2f830535e67db02ae4eca685aa9b059d3a4d7ffe
김승찬 <s24041@gsm.hs.kr>
2024-11-25 22:51:03 +0900

    style: 클래스 이름 변경

--- src/main/java/com/nextech/server/v1/domain/mission/service/impl/SuccessfulMissionInquiryServiceImpl.java
+++ src/main/java/com/nextech/server/v1/domain/mission/service/impl/SuccessfulMissionInquiryServiceImpl.java
@@ -4,7 +4,7 @@ import com.nextech.server.v1.domain.mission.dto.enums.Status;
 import com.nextech.server.v1.domain.mission.dto.response.MissionResponseDto;
 import com.nextech.server.v1.domain.mission.entity.Mission;
 import com.nextech.server.v1.domain.mission.repository.MissionRepository;
-import com.nextech.server.v1.domain.mission.service.SuccessfulMissionService;
+import com.nextech.server.v1.domain.mission.service.SuccessfulMissionInquiryService;
 import com.nextech.server.v1.global.exception.LogNotFoundException;
 import com.nextech.server.v1.global.members.entity.Members;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class SuccessfulMissionServiceImpl implements SuccessfulMissionService {
+public class SuccessfulMissionInquiryServiceImpl implements SuccessfulMissionInquiryService {
 
     private final MissionRepository missionRepository;
 
