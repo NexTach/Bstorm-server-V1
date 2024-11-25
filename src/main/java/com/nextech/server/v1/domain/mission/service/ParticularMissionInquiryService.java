926b0954948300a5ec2590acac0fe7a4ef152762
김승찬 <s24041@gsm.hs.kr>
2024-11-25 18:06:32 +0900

    style: 개행 삭제

--- src/main/java/com/nextech/server/v1/domain/mission/service/ParticularMissionInquiryService.java
+++ src/main/java/com/nextech/server/v1/domain/mission/service/ParticularMissionInquiryService.java
@@ -3,6 +3,5 @@ package com.nextech.server.v1.domain.mission.service;
 import com.nextech.server.v1.domain.mission.dto.response.MissionResponseDto;
 
 public interface ParticularMissionInquiryService {
-
     MissionResponseDto getMissionById(Long id);
 }
