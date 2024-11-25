f221a03036d49df344b0dae930d762762d1fc73e
김승찬 <s24041@gsm.hs.kr>
2024-11-25 19:33:57 +0900

    feat: findByToWardAndStatus 메서드 추가

--- src/main/java/com/nextech/server/v1/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/nextech/server/v1/domain/mission/repository/MissionRepository.java
@@ -1,5 +1,6 @@
 package com.nextech.server.v1.domain.mission.repository;
 
+import com.nextech.server.v1.domain.mission.dto.enums.Status;
 import com.nextech.server.v1.domain.mission.entity.Mission;
 import com.nextech.server.v1.global.members.entity.Members;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ import java.util.List;
 @Repository
 public interface MissionRepository extends JpaRepository<Mission, Long> {
     List<Mission> findByFromId(Members member);
+    List<Mission> findByToWardAndStatus(Members toWard, Status status);
 }
