14e1d7649b41b9f016e235567ed8a61a149eeec6
김승찬 <s24041@gsm.hs.kr>
2024-11-25 18:08:07 +0900

    feat: findByFromId 메서드 추가

--- src/main/java/com/nextech/server/v1/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/nextech/server/v1/domain/mission/repository/MissionRepository.java
@@ -1,10 +1,13 @@
 package com.nextech.server.v1.domain.mission.repository;
 
 import com.nextech.server.v1.domain.mission.entity.Mission;
+import com.nextech.server.v1.global.members.entity.Members;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MissionRepository extends JpaRepository<Mission, Long> {
-
+    List<Mission> findByFromId(Members member);
 }
