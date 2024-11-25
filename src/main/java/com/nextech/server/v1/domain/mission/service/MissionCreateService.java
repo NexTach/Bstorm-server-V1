73a1caec4d5d32be40c1150973b6321f212840c7
김승찬 <s24041@gsm.hs.kr>
2024-11-25 12:18:17 +0900

    feat: 미션등록 서비스 추가

--- src/main/java/com/nextech/server/v1/domain/mission/service/MissionCreateService.java
+++ src/main/java/com/nextech/server/v1/domain/mission/service/MissionCreateService.java
@@ -3,10 +3,7 @@ package com.nextech.server.v1.domain.mission.service;
 import com.nextech.server.v1.domain.mission.dto.request.MissionRequestDto;
 import com.nextech.server.v1.domain.mission.dto.response.MissionResponseDto;
 
-import java.util.List;
-
-public interface MissionService {
+public interface MissionCreateService {
 
     MissionResponseDto createMission(MissionRequestDto missionRequestDto);
-    List<MissionResponseDto> getAllMissions();
 }
