46e0af991bef15957c1228e69bbd35a97e5d461c
김승찬 <s24041@gsm.hs.kr>
2024-11-25 12:16:47 +0900

    feat: 전체미션정보 서비스 분리

---
+++ src/main/java/com/nextech/server/v1/domain/mission/service/AllMissionInquiryService.java
@@ -0,0 +1,10 @@
+package com.nextech.server.v1.domain.mission.service;
+
+import com.nextech.server.v1.domain.mission.dto.response.MissionResponseDto;
+
+import java.util.List;
+
+public interface AllMissionInquiryService {
+
+    List<MissionResponseDto> getAllMissions();
+}
