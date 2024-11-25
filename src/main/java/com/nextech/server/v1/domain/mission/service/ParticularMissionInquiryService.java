223a60e8ffc19d58a84f408be5f2a15a6947c114
김승찬 <s24041@gsm.hs.kr>
2024-11-25 12:20:10 +0900

    feat: 특정정보조회 서비스 구현

---
+++ src/main/java/com/nextech/server/v1/domain/mission/service/ParticularMissionInquiryService.java
@@ -0,0 +1,8 @@
+package com.nextech.server.v1.domain.mission.service;
+
+import com.nextech.server.v1.domain.mission.dto.response.MissionResponseDto;
+
+public interface ParticularMissionInquiryService {
+
+    MissionResponseDto getMissionById(Long id);
+}
