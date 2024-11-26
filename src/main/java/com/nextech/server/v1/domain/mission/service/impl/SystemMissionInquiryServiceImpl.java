d0122a64874cdabd5674b1b553ffcd435f0a0d32
김승찬 <s24041@gsm.hs.kr>
2024-11-26 20:29:46 +0900

    fix: 시스템출제미션조회 오류 수정

--- src/main/java/com/nextech/server/v1/domain/mission/service/impl/SystemMissionInquiryServiceImpl.java
+++ src/main/java/com/nextech/server/v1/domain/mission/service/impl/SystemMissionInquiryServiceImpl.java
@@ -4,11 +4,14 @@ import com.nextech.server.v1.domain.mission.dto.response.MissionResponseDto;
 import com.nextech.server.v1.domain.mission.entity.Mission;
 import com.nextech.server.v1.domain.mission.repository.MissionRepository;
 import com.nextech.server.v1.domain.mission.service.SystemMissionInquiryService;
+import com.nextech.server.v1.global.enums.Gender;
+import com.nextech.server.v1.global.enums.Roles;
 import com.nextech.server.v1.global.exception.LogNotFoundException;
 import com.nextech.server.v1.global.members.entity.Members;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,7 +23,20 @@ public class SystemMissionInquiryServiceImpl implements SystemMissionInquiryServ
 
     @Override
     public List<MissionResponseDto> getSystemMissions(Members member) {
-        List<Mission> missions = missionRepository.findByFromAndToWard("SYSTEM", member);
+        Members systemMember = new Members(
+                0L,
+                "System",
+                "+8200000000",
+                "123456",
+                1,
+                Gender.MAN,
+                Roles.ROLE_ADMIN,
+                0,
+                null,
+                null,
+                new ArrayList<>()
+        );
+        List<Mission> missions = missionRepository.findByFromAndToWard(systemMember, member);
 
         if (missions.isEmpty()) {
             throw new LogNotFoundException("No system-assigned missions found.");
