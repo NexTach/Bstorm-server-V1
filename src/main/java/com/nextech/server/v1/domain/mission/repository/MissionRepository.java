c567e605f98b42056bbeee3fa72ec9dd4e029693
김승찬 <s24041@gsm.hs.kr>
2024-11-26 13:15:51 +0900

    feat: findByFrom 메서드 추가

--- src/main/java/com/nextech/server/v1/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/nextech/server/v1/domain/mission/repository/MissionRepository.java
@@ -14,4 +14,5 @@ public interface MissionRepository extends JpaRepository<Mission, Long> {
     List<Mission> findByToWardAndStatus(Members toWard, Status status);
     List<Mission> findByFromAndStatusContains(Members member, Status status);
     List<Mission> findByFromAndToWard(String from, Members toWard);
+    List<Mission> findByFrom(Long memberId);
 }
