9cebaf3568e7c89954a56aef8956f6c7376d1e6e
snowykte0426 <snowykte0426@naver.com>
2024-11-26 22:18:39 +0900

    refactor: ``updateMissionStatus`` 메서드 파라미터 추가
    updateMissionStatus 메서드가 추가로 request 파라미터를 받도록 수정하였습니다
    
    Resolves: #13

--- src/main/java/com/nextech/server/v1/domain/mission/service/MissionStatusUpdateService.java
+++ src/main/java/com/nextech/server/v1/domain/mission/service/MissionStatusUpdateService.java
@@ -2,7 +2,8 @@ package com.nextech.server.v1.domain.mission.service;
 
 import com.nextech.server.v1.domain.mission.dto.request.MissionStatusUpdateRequestDto;
 import com.nextech.server.v1.domain.mission.dto.response.MissionResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface MissionStatusUpdateService {
-    MissionResponseDto updateMissionStatus(Long id, MissionStatusUpdateRequestDto requestDto);
+    MissionResponseDto updateMissionStatus(Long id, MissionStatusUpdateRequestDto requestDto, HttpServletRequest request);
 }
